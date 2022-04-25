1811b090f049794031e2ca8e01a613690a397003
Claudio <claudio.junior233@gmail.com>
2022-04-25 20:35:34 -0300

    feat: Criação da Entidade Sale.

---
+++ src/DevInSales.Core/Entities/Sale.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevInSales.Core.Entities
+{
+    public class Sale : Entity
+    {
+
+        public Sale(int buyerId, int sellerId, DateTime saleDate)
+        {
+
+            BuyerId = buyerId;
+            SellerId = sellerId;
+            SaleDate = saleDate;
+
+        }
+        public int BuyerId { get; private set; }
+        public int SellerId { get; private set; }
+        public DateTime SaleDate { get; private set; }
+
+
+    }
+}

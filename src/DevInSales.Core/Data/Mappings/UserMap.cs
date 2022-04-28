71862b75efef5650a841ed6bac719e6cd10b684c
Mauro Luis Paim Martins <mpmartins@bol.com.br>
2022-04-28 19:33:23 -0300

    feat: Adicionando UsersMap na Mappings

---
+++ src/DevInSales.Core/Data/Mappings/UserMap.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using DevInSales.Core.Entities;
+
+namespace DevInSales.Core.Data.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(u => u.Id);
+
+            builder.Property(u => u.Email)
+                .HasColumnType("varchar(150)")
+                .IsUnicode(false)
+                .IsRequired();
+
+            builder.Property(u => u.Password)
+                .HasColumnType("varchar(50)")
+                .IsUnicode(false)
+                .IsRequired();
+            builder.Property(u => u.BirthDate)
+                .HasColumnType("date")
+                .IsRequired();
+
+        }
+    }
+
+}

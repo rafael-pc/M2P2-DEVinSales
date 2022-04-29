521c9e4d01f5afdef16b89ae52b4a520435b3329
RafaelYukio <rafaelyut@hotmail.com>
2022-04-29 16:23:30 -0300

    feat: Criacao dos DTOs SaleResponse e SaleProductResponse

---
+++ src/DevInSales.Core/Data/Dtos/SaleProductResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevInSales.Core.Data.Dtos
+{
+    public class SaleProductResponse
+    {
+        public string ProductName { get; set; }
+        public int Amount { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Total { get; set; }
+
+        public SaleProductResponse(string productName, int amount, decimal unitPrice, decimal total)
+        {
+            ProductName = productName;
+            Amount = amount;
+            UnitPrice = unitPrice;
+            Total = total;
+        }
+    }
+}

04b810c68cc9417f1bb6195640c2ec3a15f38080
Claudio <claudio.junior233@gmail.com>
2022-05-03 18:32:52 -0300

    fix: Corrigindo nome de propriedade e gerando migration de correção.
    fix: corrigido nome da propriedade "address" da entidade Delivery;
    fix: corrigido nome da coluna DeliveryMap e gerando nova migration.

---
+++ src/DevInSales.Core/Data/Migrations/20220503212453_CorrigindoAddress.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DevInSales.Core.Data.Migrations
+{
+    public partial class CorrigindoAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deliveries_Addresses_AdressId",
+                table: "Deliveries");
+
+            migrationBuilder.RenameColumn(
+                name: "AdressId",
+                table: "Deliveries",
+                newName: "AddressId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Deliveries_AdressId",
+                table: "Deliveries",
+                newName: "IX_Deliveries_AddressId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryForecast",
+                table: "Deliveries",
+                type: "timestamptz",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deliveries_Addresses_AddressId",
+                table: "Deliveries",
+                column: "AddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Deliveries_Addresses_AddressId",
+                table: "Deliveries");
+
+            migrationBuilder.RenameColumn(
+                name: "AddressId",
+                table: "Deliveries",
+                newName: "AdressId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Deliveries_AddressId",
+                table: "Deliveries",
+                newName: "IX_Deliveries_AdressId");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeliveryForecast",
+                table: "Deliveries",
+                type: "timestamp",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamptz");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Deliveries_Addresses_AdressId",
+                table: "Deliveries",
+                column: "AdressId",
+                principalTable: "Addresses",
+                principalColumn: "Id");
+        }
+    }
+}

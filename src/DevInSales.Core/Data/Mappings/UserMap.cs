534f82ccab6b49247a1b1e5fcf4dc9fdd51a437d
Claydson <71528039+sicklex@users.noreply.github.com>
2022-05-06 14:43:30 -0300

    Fix: Update user mapping

--- src/DevInSales.Core/Data/Mappings/UserMap.cs
+++ src/DevInSales.Core/Data/Mappings/UserMap.cs
@@ -12,6 +12,10 @@ namespace DevInSales.Core.Data.Mappings
 
             builder.HasKey(u => u.Id);
 
+            builder.Property(u => u.Name)
+                .HasColumnType("varchar(255)")
+                .IsRequired();
+
             builder.Property(u => u.Email)
                 .HasColumnType("varchar(150)")
                 .IsUnicode(false)
@@ -24,7 +28,7 @@ namespace DevInSales.Core.Data.Mappings
             builder.Property(u => u.BirthDate)
                 .HasColumnType("date")
                 .IsRequired();
-            
+
             builder.HasData(
                 new User(1, "Allie.Spencer@manuel.us", "661845", "Allie Spencer", new DateTime(1980, 10, 11)),
                 new User(2, "Earnest@kari.biz", "800631", "Lemuel Witting", new DateTime(1980, 10, 11)),
